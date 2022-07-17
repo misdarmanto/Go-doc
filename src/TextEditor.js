a3d1acd4e33f61cc3955cad3e2a1b6dc4af6a639
webpojectitera123 <web.itera@gmail.com>
2022-07-17 23:42:55 +0700

    first commit

---
+++ src/TextEditor.js
@@ -0,0 +1,75 @@
+import { useCallback, useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
+import { TOOLBAR } from "./Toolbar";
+
+export default function TextEditor() {
+  const { id: documentId } = useParams();
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+
+  useEffect(() => {
+    const s = io("https://go-docdoc.herokuapp.com/");
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
+
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handleUpdate = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("receive-changes", handleUpdate);
+
+    return () => {
+      socket.off("receive-changes", handleUpdate);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR },
+    });
+    q.disable();
+    q.setText("Loading...");
+    setQuill(q);
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
+}
---
+++ src/Toolbar.js
@@ -0,0 +1,13 @@
+export const TOOLBAR = [
+    [{ font: [] }],
+    [{ align: [] }],
+    ["bold", "italic", "underline", "strike"],
+    ["blockquote", "code-block"],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ direction: "rtl" }],
+    [{ color: [] }, { background: [] }], 
+    ["clean"],
+  ];
